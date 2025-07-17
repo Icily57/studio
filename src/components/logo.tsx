72dcf41f0af6cbe6d254fb8303c45ba9517fd80a
Icily57 <icilyweird57@gmail.com>
2025-07-17 21:27:18 +0300

    Refactor code structure for improved readability and maintainability

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,9 +1,9 @@
-import { Layers3 } from "lucide-react";
+
 
 export function Logo() {
   return (
     <div className="flex items-center gap-2">
-      <img src="./images/logo.png"></img>
+      <img src="../assets/images/logo.png" alt="Logo" className="h-8 w-8 "></img>
       <span className="font-headline text-xl font-bold">FolioForge</span>
     </div>
   );
