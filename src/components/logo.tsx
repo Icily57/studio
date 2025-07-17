8411c8218bb5fa94e189652f278ac8dd868be5a9
MIKAELA KAMUNYA <icilyweird57@gmail.com>
2025-07-17 17:58:16 +0000

    new

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import { Layers3 } from "lucide-react";
 export function Logo() {
   return (
     <div className="flex items-center gap-2">
-      <Layers3 className="h-6 w-6 text-accent" />
+      <img src="./images/logo.png"></img>
       <span className="font-headline text-xl font-bold">FolioForge</span>
     </div>
   );
